db0f384262157473d43386bf517d462d3bb05952
isle88 <hyunjungkim2014@gmail.com>
2022-12-07 19:48:13 +0000

    added path to profile

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,21 +1,29 @@
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { Profile } from "routes/Profile";
 import { Auth } from "../routes/Auth";
 import { Home } from "../routes/Home";
+import { Navigation } from "./Navigation";
 
 export const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home />
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
           </>
         ) : (
-          <Route exact path="/">
-            <Auth />
-          </Route>
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+          </>
         )}
       </Switch>
     </Router>
