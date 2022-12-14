6b74afa45972bb6bee66d214c18bfe9208525465
isle88 <hyunjungkim2014@gmail.com>
2022-12-14 11:48:22 +0000

    added uploading file feature

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
+import { v4 as uuidv4 } from "uuid";
 import { Nweet } from "components/Nweet";
-import { dbService } from "fbase";
-import React, { useEffect, useState } from "react";
+import { dbService, storageService } from "fbase";
+import React, { useEffect, useRef, useState } from "react";
 
 export const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
+  const [attachment, setAttachment] = useState("");
 
   useEffect(() => {
     dbService
@@ -21,18 +23,43 @@ export const Home = ({ userObj }) => {
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    await dbService.collection("nweets").add({
+    let attachmentUrl = "";
+    if (attachment !== "") {
+      const attachmentRef = storageService
+        .ref()
+        .child(`${userObj.uid}/${uuidv4()}`);
+      const response = await attachmentRef.putString(attachment, "data_url");
+      attachmentUrl = await response.ref.getDownloadURL();
+    }
+    const nweetObj = {
       text: nweet,
       createdAt: Date.now(),
       userId: userObj.uid,
-    });
+      attachmentUrl,
+    };
+    await dbService.collection("nweets").add(nweetObj);
     setNweet("");
+    onClearAttachment()
   };
   const onChange = (event) => {
     const { value } = event.target;
     setNweet(value);
   };
-
+  const onFileChange = (event) => {
+    const { files } = event.target;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const { result } = finishedEvent.currentTarget;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+  const fileInput = useRef();
+  const onClearAttachment = () => {
+    fileInput.current.value = "";
+    setAttachment("");
+  };
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -43,7 +70,24 @@ export const Home = ({ userObj }) => {
           placeholder="what's on your mind?"
           maxLength={120}
         />
+        <input
+          type="file"
+          accept="image/*"
+          onChange={onFileChange}
+          ref={fileInput}
+        />
         <input type="submit" value="Nweet" />
+        {attachment && (
+          <div>
+            <img
+              src={attachment}
+              width="70px"
+              height="70px"
+              alt="uploaded from user"
+            />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {nweets.map((nweet) => (
