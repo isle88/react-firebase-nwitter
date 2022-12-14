2c96d2d18556684fac379e8efcc8e6024977ce95
isle88 <hyunjungkim2014@gmail.com>
2022-12-14 19:00:53 +0000

    added refreshUser function

--- src/components/App.js
+++ src/components/App.js
@@ -8,15 +8,33 @@ function App() {
 
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
-      user ? setUserObj(user) : setUserObj(null)
+      if (user) {
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args),
+        });
+      }
       setInit(true);
     });
   }, []);
+  const refreshUser = () => {
+    const user = authService.currentUser
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args),
+    });;
+  };
 
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+        <AppRouter
+          refreshUser={refreshUser}
+          isLoggedIn={Boolean(userObj)}
+          userObj={userObj}
+        />
       ) : (
         "Initialising"
       )}
