3fe067567dd081a3056c8b1145e6a7d15ac80ba3
isle88 <hyunjungkim2014@gmail.com>
2022-12-14 16:59:10 +0000

    added queries

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,23 @@
-import { authService } from "fbase";
-import React from "react";
+import { authService, dbService } from "fbase";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-export const Profile = () => {
+export const Profile = ({ userObj }) => {
   const history = useHistory();
   const onSignOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+  const getMyNweet = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("userId", "==", userObj.uid)
+      .orderBy("createdAt", "desc")
+      .get();
+  };
+  useEffect(() => {
+    getMyNweet();
+  }, []);
   return (
     <>
       <button onClick={onSignOutClick}>Sign Out</button>
